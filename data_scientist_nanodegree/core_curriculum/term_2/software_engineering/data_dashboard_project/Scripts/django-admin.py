a2e86fddd6a890947c7955d095b31e47e8f1eecc
Simone Rigoni <simone.rigoni01@gmail.com>
2020-04-22 14:39:31 +0200

    Delete venv objects

--- data_scientist_nanodegree/core_curriculum/term_2/software_engineering/data_dashboard_project/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\simon\source\repos\udacity\data_scientist_nanodegree\core_curriculum\term_2\software_engineering\data_dashboard_project\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
